bea9990307d5989f84ead9b7109bc4629ebc2c15
Antara Mahmud <antaramahmud@antaras-mbp.mynetworksettings.com>
2023-07-06 00:51:56 -0400

    Adding Encryption for Password

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 //jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 
 const app = express();
@@ -14,11 +14,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect("mongodb://localhost:27017/userDB");
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-};
+});
 
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
